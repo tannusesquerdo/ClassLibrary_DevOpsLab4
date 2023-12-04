228959f90a2fdf4ab97adf2509715b0e8a3366b2
tannusesquerdo <tannusewrton@gmail.com>
2023-12-04 17:46:29 -0500

    Add ShowCase Project wiht the NuGet Package

---
+++ ShowCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+            {
+                ResetConsole();
+            }
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " + $"{(input.StartWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        void ResetConsole()
+        {
+            if (row > 0) 
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only o exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
