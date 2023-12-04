93be4cdd140264262d386e0c76972a34f4b2d6d7
tannusesquerdo <tannusewrton@gmail.com>
2023-12-04 16:11:20 -0500

    Add project files.

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace UtilityLibraries;
+public static class StringLibrary
+{
+    public static bool StartWithUpper(this string? str) 
+    {
+        if (string.IsNullOrEmpty(str)) return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+
+}
